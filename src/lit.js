f4442cc2bed5848dfbe762d5c822979a83dd79f2
William Cotton <williamcotton@gmail.com>
2013-07-05 01:39:18 +0200

    moved github login stuff here from server

--- src/lit.js
+++ src/lit.js
@@ -67,6 +67,33 @@
 
     }
   });
+  
+  var litLogin = function() {
+    
+    var login_button = document.createElement("div");
+    login_button.classList.add("login");
+
+    document.body.appendChild(login_button);
+
+    login_button.addEventListener("click", function() {
+      window.open('https://github.com' + 
+        '/login/oauth/authorize' + 
+        '?client_id=f497d63f8657e29d73cc' +
+        '&scope=gist');
+    });
+
+    window.addEventListener('message', function (event) {
+    	var code = event.data;
+      var loginRequest = new XMLHttpRequest();
+      loginRequest.open("get", '/oauth_token?code=' + code, true);
+      loginRequest.onload = function(request) {
+        var github_details = JSON.parse(request.target.response);
+        console.log(github_details);
+      };
+      loginRequest.send();
+    });
+    
+  };
 
   var lit = function(package_definition, name, deps, callback) {
 
@@ -149,5 +176,47 @@
   };
   
   root.lit = lit;
+  
+  var docCookies = {
+    getItem: function (sKey) {
+      return decodeURI(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURI(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+            break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+            break;
+          case Date:
+            sExpires = "; expires=" + vEnd.toGMTString();
+            break;
+        }
+      }
+      document.cookie = encodeURI(sKey) + "=" + encodeURI(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+      return true;
+    },
+    removeItem: function (sKey, sPath) {
+      if (!sKey || !this.hasItem(sKey)) { return false; }
+      document.cookie = encodeURI(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sPath ? "; path=" + sPath : "") + (sDomain ? "; domain=" + sDomain : "");
+      return true;
+    },
+    hasItem: function (sKey) {
+      return (new RegExp("(?:^|;\\s*)" + encodeURI(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: /* optional method: you can safely remove it! */ function () {
+      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = decodeURI(aKeys[nIdx]); }
+      return aKeys;
+    }
+  };
+  
+  console.log(docCookies.getItem("lit!username"));
+  
+  litLogin();
   
 })(this);
